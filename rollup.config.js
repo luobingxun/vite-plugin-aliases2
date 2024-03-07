9510df931d557cf301db5657dc4a8d7f5822ff01
mingwei.li <mingwei.li@tarsocial.com>
2024-03-07 15:19:06 +0800

    feat: add build minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'rollup';
+import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import cjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 
 export default defineConfig({
   input: './src/index.ts',
-  plugins: [resolve(), cjs(), typescript()],
+  plugins: [resolve(), cjs(), typescript(), terser()],
   output: [
     {
       file: './dist/index.cjs',
